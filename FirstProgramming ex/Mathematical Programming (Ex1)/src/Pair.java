968d8e9ee486a6ab53557f39728c25d9d2b8f047
mattzav <matteozav@hotmail.com>
2020-04-18 14:58:23 +0200

    Mtz, Scf, Mcf implementations

---
+++ FirstProgramming ex/Mathematical Programming (Ex1)/src/Pair.java
@@ -0,0 +1,29 @@
+
+public class Pair {
+
+	private int endpoint_1;
+	private int endpoint_2;
+
+	public int getEndpoint_1() {
+		return endpoint_1;
+	}
+
+	public void setEndpoint_1(int endpoint_1) {
+		this.endpoint_1 = endpoint_1;
+	}
+
+	public int getEndpoint_2() {
+		return endpoint_2;
+	}
+
+	public void setEndpoint_2(int endpoint_2) {
+		this.endpoint_2 = endpoint_2;
+	}
+
+	public Pair(int endpoint_1, int endpoint_2) {
+		super();
+		this.endpoint_1 = endpoint_1;
+		this.endpoint_2 = endpoint_2;
+	}
+
+}
